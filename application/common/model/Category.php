535f50e9efa161539a6354e384386669c971731b
webjust <604854119@qq.com>
2017-07-11 01:18:03 +0800

    编辑更新功能

--- application/common/model/Category.php
+++ application/common/model/Category.php
@@ -1,9 +1,44 @@
 <?php
+
 namespace app\common\model;
 
 use think\Model;
 
 class Category extends Model
 {
+    protected $autoWriteTimestamp = true;
 
+    public function add($data)
+    {
+        $data['status'] = 1;
+        return $this->save($data);
+    }
+
+    public function getNormalFirstCategory()
+    {
+        $data = [
+            'status' => 1,
+            'parent_id' => 0,
+        ];
+        $order = ['id' => 'desc'];
+        return $this->where($data)
+            ->order($order)
+            ->select();
+    }
+
+    public function getFirstCategorys($parent_id = 0)
+    {
+        $data = [
+            'status' => ['neq', '-1'],
+            'parent_id' => $parent_id,
+        ];
+        $order = ['id' => 'desc'];
+        $result = $this->where($data)
+            ->order($order)
+            ->paginate();
+
+//        echo $this->getLastSql();
+
+        return $result;
+    }
 }
